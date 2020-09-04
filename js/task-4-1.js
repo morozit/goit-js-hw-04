096d31b2731c0c0406eee80f068c734a4d042923
Roman <65686226+morozit@users.noreply.github.com>
2020-09-04 15:23:35 +0300

    Update task-4-1.js

--- js/task-4-1.js
+++ js/task-4-1.js
@@ -1,7 +1,7 @@
 // TODO: element += index
-const addIndex = (element, index) => (element += index);
+const addIndex = (element, index) => element + index;
 
-const subIndex = (element, index) => (element -= index);
+const subIndex = (element, index) => element - index;
 
 function mapArray(array, cb) {
   "use strict";
@@ -9,11 +9,14 @@ function mapArray(array, cb) {
   const numbers = new Array(array.length);
 
   for (let i = 0; i < array.length; i += 1) {
-    const element1 = array[i];
-    const index1 = i;
-    // TODO: ты говоришь вызвать функцию cb, аргументами которой будут заявленные выше в этой же функции (mapArray) элемент и индекс и сделать с ними то, что заявлено в функции cb
+    // TODO: заміняє 3 слідуючих рядка
+    // numbers[i] = cb(array[i], i);
+    const element = array[i];
+    const index = i;
 
-    numbers[i] = cb(element1, index1);
+    // TODO: numbers[i]   ===>  вместо каждого итого элемента массива мы получаем результат функции
+    // TODO: ты говоришь вызвать функцию cb, аргументами которой будут заявленные выше в этой же функции (mapArray) элемент и индекс и сделать с ними то, что заявлено в функции cb
+    numbers[i] = cb(element, index);
   }
 
   return numbers;
